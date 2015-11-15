1bf50cf94dc1e99595ce1f37b19bae3847d4052d
Mark Chadwick <m.chadwick@gns.cri.nz>
2015-11-16 12:20:26 +1300

    add datalogger to sensor connector

---
+++ meta/connection.go
@@ -0,0 +1,50 @@
+package meta
+
+import (
+	"sort"
+	"time"
+)
+
+type Connection struct {
+	Station   string    `csv:"Station Code"`
+	Location  string    `csv:"Location Code"`
+	Place     string    `csv:"Datalogger Place"`
+	Offset    int16     `csv:"Offset"`
+	Length    int16     `csv:"Length"`
+	StartTime time.Time `csv:"Start Date"`
+	EndTime   time.Time `csv:"End Date"`
+}
+
+type Connections []Connection
+
+func (c Connections) Len() int      { return len(c) }
+func (c Connections) Swap(i, j int) { c[i], c[j] = c[j], c[i] }
+func (c Connections) Less(i, j int) bool {
+	switch {
+	case c[i].Station < c[j].Station:
+		return true
+	case c[i].Station > c[j].Station:
+		return false
+	case c[i].Location < c[j].Location:
+		return true
+	case c[i].Location > c[j].Location:
+		return false
+	case c[i].Place < c[j].Place:
+		return true
+	case c[i].Place > c[j].Place:
+		return false
+	case c[i].StartTime.Before(c[j].StartTime):
+		return true
+	case c[i].StartTime.After(c[j].StartTime):
+		return false
+	case c[i].Offset < c[j].Offset:
+		return true
+	case c[i].Offset > c[j].Offset:
+		return false
+	default:
+		return false
+	}
+}
+
+func (c Connections) List()      {}
+func (c Connections) Sort() List { sort.Sort(c); return c }
