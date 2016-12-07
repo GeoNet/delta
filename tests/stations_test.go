d6e9bc3597797dee675913c18631dfdf99d4ade1
Geoff Clitheroe <g.clitheroe@gns.cri.nz>
2016-12-07 19:22:18 +1300

    GeoJSON of gauge Station locations.

--- tests/stations_test.go
+++ tests/stations_test.go
@@ -8,6 +8,8 @@ import (
 	"github.com/golang/protobuf/proto"
 	"os"
 	"io/ioutil"
+	"fmt"
+	"bytes"
 )
 
 func TestStations(t *testing.T) {
@@ -31,6 +33,7 @@ func TestStations(t *testing.T) {
 }
 
 // TestStationsProto creates a Protobuf file of Stations.
+// Creates a GeoJSON file of gauge locations.
 func TestStationsProto(t *testing.T) {
 	var networks meta.NetworkList
 
@@ -92,13 +95,54 @@ func TestStationsProto(t *testing.T) {
 		t.Error(err)
 	}
 
-	t.Log(s)
-
 	if err := os.MkdirAll(apiDir, 0777); err != nil {
 		t.Error(err)
 	}
 
 	if err := ioutil.WriteFile(apiDir + "/stations.pb", b, 0644); err != nil {
+		t.Error(err)
+	}
+
+	// GeoJSON for sea level gauges.
+	// Output the Station long/lat.  This is not the same as the
+	// sensor locations.  Will need to consider adding this as a
+	// separate layer (station versus location).
+	// Assume that the network code is enough to ID the gauges.
+	var bf bytes.Buffer
+
+	bf.WriteString(`{"type": "FeatureCollection","features": [`)
+
+	f := false
+	for _, v := range s.Stations {
+		if v.GetSpan() == nil || v.GetPoint() == nil || v.GetNetwork() == nil || v.Network.Restricted {
+			continue
+		}
+
+		if v.Network.Code != "TG" {
+			continue
+		}
+
+		if f {
+			bf.WriteString(`,`)
+		}
+		f = true
+
+		bf.WriteString(`{"type":"Feature","geometry":{"type": "Point","coordinates": [`)
+		bf.WriteString(fmt.Sprintf("%f,%f", v.Point.Longitude, v.Point.Latitude))
+		bf.WriteString(`]},"properties":{`)
+		bf.WriteString(fmt.Sprintf("\"code\":\"%s\"", v.Code))
+		bf.WriteString(fmt.Sprintf(",\"name\":\"%s\"", v.Name))
+		bf.WriteString(fmt.Sprintf(",\"datum\":\"%s\"", v.Point.Datum))
+		bf.WriteString(fmt.Sprintf(",\"elevation\":%f", v.Point.Elevation))
+		bf.WriteString(fmt.Sprintf(",\"start\":\"%d\"", v.Span.Start))
+		bf.WriteString(fmt.Sprintf(",\"end\":\"%d\"", v.Span.End))
+		bf.WriteString(fmt.Sprintf(",\"network\":\"%s\"", v.Network.External))
+		bf.WriteString(`}}`)
+	}
+
+	bf.WriteString(`]}`)
+
+	if err := ioutil.WriteFile(apiDir + "/gauges.geojson", bf.Bytes(), 0644); err != nil {
 		t.Error(err)
 	}
 }
