8a7e48aef54b91b0527fb984d66d0f6d9b71d32b
Mark Chadwick <m.chadwick@gns.cri.nz>
2017-06-01 14:51:10 +1200

    now use monument dates rather than associated mark dates

--- tests/marks_test.go
+++ tests/marks_test.go
@@ -3,20 +3,30 @@ package delta_test
 import (
 	"testing"
 
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"github.com/GeoNet/delta"
 	"github.com/GeoNet/delta/meta"
+	"github.com/golang/protobuf/proto"
 	"io/ioutil"
 	"os"
-	"github.com/GeoNet/delta"
-	"github.com/golang/protobuf/proto"
-	"encoding/json"
-	"bytes"
-	"fmt"
 	"time"
 )
 
 const apiDir = "../.tmp/api/delta"
 
 func TestMarks(t *testing.T) {
+
+	var monuments meta.MonumentList
+	if err := meta.LoadList("../network/monuments.csv", &monuments); err != nil {
+		t.Fatal(err)
+	}
+
+	lookup := make(map[string]meta.Monument)
+	for _, m := range monuments {
+		lookup[m.Mark] = m
+	}
 
 	var marks meta.MarkList
 	t.Log("Load network marks file")
@@ -34,6 +44,16 @@ func TestMarks(t *testing.T) {
 		}
 	}
 
+	for _, m := range marks {
+		if l, ok := lookup[m.Code]; ok {
+			if m.Start.Before(l.Start) {
+				t.Errorf("mark established before monument %s: mark=%s monument=%s", m.Code, m.Start, l.Start)
+			}
+			if l.End.Before(m.End) {
+				t.Errorf("mark continues after monument removed %s: mark=%s monument=%s", m.Code, m.End, l.End)
+			}
+		}
+	}
 }
 
 // TestMarksProto creates protobuf and JSON files of Marks.
@@ -42,25 +62,25 @@ func TestMarks(t *testing.T) {
 // marks.pb - fully hydrated protobuf will all GNSS Mark information.
 // marks.json - JSON version of marks.pb (for use in browsers).
 // marks.geojson - GeoJSON of Mark locations.
- func TestMarksProto(t *testing.T) {
+func TestMarksProto(t *testing.T) {
 	var networks meta.NetworkList
 
 	if err := meta.LoadList("../network/networks.csv", &networks); err != nil {
 		t.Error(err)
 	}
 
-	 var net = make(map[string]*delta.Network)
+	var net = make(map[string]*delta.Network)
 
-	 for _, v := range networks {
-		 n := delta.Network{
-			 Code: v.Code,
-			 External: v.External,
-			 Description: v.Description,
-			 Restricted: v.Restricted,
-		 }
+	for _, v := range networks {
+		n := delta.Network{
+			Code:        v.Code,
+			External:    v.External,
+			Description: v.Description,
+			Restricted:  v.Restricted,
+		}
 
-		 net[v.Code] = &n
-	 }
+		net[v.Code] = &n
+	}
 
 	var marks meta.MarkList
 	if err := meta.LoadList("../network/marks.csv", &marks); err != nil {
@@ -77,295 +97,295 @@ func TestMarks(t *testing.T) {
 
 	for _, v := range marks {
 		pt := delta.Point{
-			Longitude:v.Longitude,
-			Latitude:v.Latitude,
-			Elevation:v.Elevation,
-			Datum:v.Datum,
+			Longitude: v.Longitude,
+			Latitude:  v.Latitude,
+			Elevation: v.Elevation,
+			Datum:     v.Datum,
 		}
 
 		s := delta.Span{
-			Start:v.Start.Unix(),
-			End:v.End.Unix(),
+			Start: v.Start.Unix(),
+			End:   v.End.Unix(),
 		}
 
 		mk := delta.Mark{
-			Code:v.Code,
-			Name:v.Name,
+			Code:    v.Code,
+			Name:    v.Name,
 			Network: net[v.Network],
-			Point:&pt,
-			Span:&s,
+			Point:   &pt,
+			Span:    &s,
 		}
 
 		m.Marks[mk.Code] = &mk
 	}
 
-	 var monuments meta.MonumentList
-	 if err := meta.LoadList("../network/monuments.csv", &monuments); err != nil {
-		 t.Error(err)
-	 }
+	var monuments meta.MonumentList
+	if err := meta.LoadList("../network/monuments.csv", &monuments); err != nil {
+		t.Error(err)
+	}
 
-	 for _,v := range monuments {
-		 s := delta.Span{
-			 Start:v.Start.Unix(),
-			 End:v.End.Unix(),
-		 }
+	for _, v := range monuments {
+		s := delta.Span{
+			Start: v.Start.Unix(),
+			End:   v.End.Unix(),
+		}
 
-		 mn := delta.Monument{
-			 DomesNumber:v.DomesNumber,
-			 MarkType:v.MarkType,
-			 Type     :v.Type,
-			 GroundRelationship:v.GroundRelationship,
-			 FoundationType:v.FoundationType,
-			 FoundationDepth:v.FoundationDepth,
-			 Bedrock: v.Bedrock,
-			 Geology: v.Geology,
-			 Span: &s,
-		 }
-		 if _, ok := m.Marks[v.Mark]; ok {
-			 m.Marks[v.Mark].Monument = &mn
-		 }
-	 }
+		mn := delta.Monument{
+			DomesNumber:        v.DomesNumber,
+			MarkType:           v.MarkType,
+			Type:               v.Type,
+			GroundRelationship: v.GroundRelationship,
+			FoundationType:     v.FoundationType,
+			FoundationDepth:    v.FoundationDepth,
+			Bedrock:            v.Bedrock,
+			Geology:            v.Geology,
+			Span:               &s,
+		}
+		if _, ok := m.Marks[v.Mark]; ok {
+			m.Marks[v.Mark].Monument = &mn
+		}
+	}
 
-	 var antennas meta.InstalledAntennaList
-	 if err := meta.LoadList("../install/antennas.csv", &antennas); err != nil {
-		 t.Error(err)
-	 }
+	var antennas meta.InstalledAntennaList
+	if err := meta.LoadList("../install/antennas.csv", &antennas); err != nil {
+		t.Error(err)
+	}
 
-	 for _, v := range antennas {
-		 e := delta.Equipment{
-			 Make: v.Make,
-			 Model: v.Model,
-			 Serial: v.Serial,
-		 }
-		 o := delta.Offset{
-			 Vertical:v.Vertical,
-			 North:v.North,
-			 East:v.East,
-		 }
-		 s := delta.Span{
-			 Start:v.Start.Unix(),
-			 End:v.End.Unix(),
-		 }
-		 a := delta.InstalledAntenna{
-			 Equipment:&e,
-			 Offset:&o,
-			 Span:&s,
-			 Azimuth:v.Azimuth,
-		 }
+	for _, v := range antennas {
+		e := delta.Equipment{
+			Make:   v.Make,
+			Model:  v.Model,
+			Serial: v.Serial,
+		}
+		o := delta.Offset{
+			Vertical: v.Vertical,
+			North:    v.North,
+			East:     v.East,
+		}
+		s := delta.Span{
+			Start: v.Start.Unix(),
+			End:   v.End.Unix(),
+		}
+		a := delta.InstalledAntenna{
+			Equipment: &e,
+			Offset:    &o,
+			Span:      &s,
+			Azimuth:   v.Azimuth,
+		}
 
-		 if _, ok := m.Marks[v.Mark]; ok {
-			 m.Marks[v.Mark].InstalledAntenna = append(m.Marks[v.Mark].InstalledAntenna, &a)
-		 }
-	 }
+		if _, ok := m.Marks[v.Mark]; ok {
+			m.Marks[v.Mark].InstalledAntenna = append(m.Marks[v.Mark].InstalledAntenna, &a)
+		}
+	}
 
-	 var radomes meta.InstalledRadomeList
-	 if err := meta.LoadList("../install/radomes.csv", &radomes); err != nil {
-		 t.Error(err)
-	 }
+	var radomes meta.InstalledRadomeList
+	if err := meta.LoadList("../install/radomes.csv", &radomes); err != nil {
+		t.Error(err)
+	}
 
-	 for _, v := range radomes {
-		 e := delta.Equipment{
-			 Make: v.Make,
-			 Model: v.Model,
-			 Serial: v.Serial,
-		 }
-		 s := delta.Span{
-			 Start:v.Start.Unix(),
-			 End:v.End.Unix(),
-		 }
-		 r := delta.InstalledRadome{
-			 Equipment: &e,
-			 Span: &s,
-		 }
-		 if _, ok := m.Marks[v.Mark]; ok {
-			 m.Marks[v.Mark].InstalledRadome = append(m.Marks[v.Mark].InstalledRadome, &r)
-		 }
-	 }
+	for _, v := range radomes {
+		e := delta.Equipment{
+			Make:   v.Make,
+			Model:  v.Model,
+			Serial: v.Serial,
+		}
+		s := delta.Span{
+			Start: v.Start.Unix(),
+			End:   v.End.Unix(),
+		}
+		r := delta.InstalledRadome{
+			Equipment: &e,
+			Span:      &s,
+		}
+		if _, ok := m.Marks[v.Mark]; ok {
+			m.Marks[v.Mark].InstalledRadome = append(m.Marks[v.Mark].InstalledRadome, &r)
+		}
+	}
 
-	 var firmwares meta.FirmwareHistoryList
-	 if err := meta.LoadList("../install/firmware.csv", &firmwares); err != nil {
-		 t.Error(err)
-	 }
+	var firmwares meta.FirmwareHistoryList
+	if err := meta.LoadList("../install/firmware.csv", &firmwares); err != nil {
+		t.Error(err)
+	}
 
-	 var fw = make(map[delta.Equipment]delta.Receiver)
+	var fw = make(map[delta.Equipment]delta.Receiver)
 
-	 for _, v := range firmwares {
-		 e := delta.Equipment{
-			 Make: v.Make,
-			 Model: v.Model,
-			 Serial: v.Serial,
-		 }
-		 s := delta.Span{
-			 Start:v.Start.Unix(),
-			 End:v.End.Unix(),
-		 }
-		 f := delta.Firmware{
-			 Version: v.Version,
-			 Notes: v.Notes,
-			 Span: &s,
-		 }
+	for _, v := range firmwares {
+		e := delta.Equipment{
+			Make:   v.Make,
+			Model:  v.Model,
+			Serial: v.Serial,
+		}
+		s := delta.Span{
+			Start: v.Start.Unix(),
+			End:   v.End.Unix(),
+		}
+		f := delta.Firmware{
+			Version: v.Version,
+			Notes:   v.Notes,
+			Span:    &s,
+		}
 
-		 rx := fw[e]
-		 rx.Equipment =&e
-		 rx.Firmware = append(rx.Firmware, &f)
-		 fw[e] = rx
-	 }
+		rx := fw[e]
+		rx.Equipment = &e
+		rx.Firmware = append(rx.Firmware, &f)
+		fw[e] = rx
+	}
 
-	 var receivers meta.DeployedReceiverList
-	 if err := meta.LoadList("../install/receivers.csv", &receivers); err != nil {
-		 t.Error(err)
-	 }
+	var receivers meta.DeployedReceiverList
+	if err := meta.LoadList("../install/receivers.csv", &receivers); err != nil {
+		t.Error(err)
+	}
 
-	 for _,v := range receivers {
-		 e := delta.Equipment{
-			 Make: v.Make,
-			 Model: v.Model,
-			 Serial: v.Serial,
-		 }
-		 s := delta.Span{
-			 Start:v.Start.Unix(),
-			 End:v.End.Unix(),
-		 }
+	for _, v := range receivers {
+		e := delta.Equipment{
+			Make:   v.Make,
+			Model:  v.Model,
+			Serial: v.Serial,
+		}
+		s := delta.Span{
+			Start: v.Start.Unix(),
+			End:   v.End.Unix(),
+		}
 
-		 rx, ok := fw[e]
-		 if !ok {
-			 t.Errorf("no firware for %v", e)
-		 }
+		rx, ok := fw[e]
+		if !ok {
+			t.Errorf("no firware for %v", e)
+		}
 
-		 d := delta.DeployedReceiver{
-			 Receiver: &rx,
-			 Span: &s,
-		 }
+		d := delta.DeployedReceiver{
+			Receiver: &rx,
+			Span:     &s,
+		}
 
-		 if _, ok := m.Marks[v.Mark]; ok {
-			 m.Marks[v.Mark].DeployedReceiver = append(m.Marks[v.Mark].DeployedReceiver, &d)
-		 }
-	 }
+		if _, ok := m.Marks[v.Mark]; ok {
+			m.Marks[v.Mark].DeployedReceiver = append(m.Marks[v.Mark].DeployedReceiver, &d)
+		}
+	}
 
-	 var sessions meta.SessionList
-	 if err := meta.LoadList("../install/sessions.csv", &sessions); err != nil {
-		 t.Error(err)
-	 }
+	var sessions meta.SessionList
+	if err := meta.LoadList("../install/sessions.csv", &sessions); err != nil {
+		t.Error(err)
+	}
 
-	 for _, v := range sessions {
-		 s := delta.Span{
-			 Start:v.Start.Unix(),
-			 End:v.End.Unix(),
-		 }
-		 se := delta.Session{
-			 Operator: v.Operator,
-			 Agency          :v.Agency,
-			 Model           : v.Model,
-			 SatelliteSystem : v.SatelliteSystem,
-			 Interval        :v.Interval.Nanoseconds(),
-			 ElevationMask: v.ElevationMask,
-			 HeaderComment: v.HeaderComment,
-			 Span: &s,
-		 }
-		 if _, ok := m.Marks[v.Mark]; ok {
-			 m.Marks[v.Mark].Session = append(m.Marks[v.Mark].Session, &se)
-		 }
-	 }
+	for _, v := range sessions {
+		s := delta.Span{
+			Start: v.Start.Unix(),
+			End:   v.End.Unix(),
+		}
+		se := delta.Session{
+			Operator:        v.Operator,
+			Agency:          v.Agency,
+			Model:           v.Model,
+			SatelliteSystem: v.SatelliteSystem,
+			Interval:        v.Interval.Nanoseconds(),
+			ElevationMask:   v.ElevationMask,
+			HeaderComment:   v.HeaderComment,
+			Span:            &s,
+		}
+		if _, ok := m.Marks[v.Mark]; ok {
+			m.Marks[v.Mark].Session = append(m.Marks[v.Mark].Session, &se)
+		}
+	}
 
-	 var metsensors meta.InstalledMetSensorList
-	 if err := meta.LoadList("../install/metsensors.csv", &metsensors); err != nil {
-		 t.Error(err)
-	 }
+	var metsensors meta.InstalledMetSensorList
+	if err := meta.LoadList("../install/metsensors.csv", &metsensors); err != nil {
+		t.Error(err)
+	}
 
-	 for _, v := range metsensors {
-		 e := delta.Equipment{
-			 Make: v.Make,
-			 Model: v.Model,
-			 Serial: v.Serial,
-		 }
-		 s := delta.Span{
-			 Start:v.Start.Unix(),
-			 End:v.End.Unix(),
-		 }
-		 p := delta.Point{
-			 Longitude:v.Longitude,
-			 Latitude:v.Latitude,
-			 Elevation:v.Elevation,
-			 Datum:v.Datum,
-		 }
-		 ms := delta.InstalledMetSensor{
-			 Equipment: &e,
-			 Span: &s,
-			 Point: &p,
-			 IMSComment:v.IMSComment,
-		 }
-		 if _, ok := m.Marks[v.Mark]; ok {
-			 m.Marks[v.Mark].InstalledMetSensor = append(m.Marks[v.Mark].InstalledMetSensor, &ms)
-		 }
-	 }
+	for _, v := range metsensors {
+		e := delta.Equipment{
+			Make:   v.Make,
+			Model:  v.Model,
+			Serial: v.Serial,
+		}
+		s := delta.Span{
+			Start: v.Start.Unix(),
+			End:   v.End.Unix(),
+		}
+		p := delta.Point{
+			Longitude: v.Longitude,
+			Latitude:  v.Latitude,
+			Elevation: v.Elevation,
+			Datum:     v.Datum,
+		}
+		ms := delta.InstalledMetSensor{
+			Equipment:  &e,
+			Span:       &s,
+			Point:      &p,
+			IMSComment: v.IMSComment,
+		}
+		if _, ok := m.Marks[v.Mark]; ok {
+			m.Marks[v.Mark].InstalledMetSensor = append(m.Marks[v.Mark].InstalledMetSensor, &ms)
+		}
+	}
 
-	 // output files
+	// output files
 
-	 if err := os.MkdirAll(apiDir, 0777); err != nil {
-		 t.Error(err)
-	 }
+	if err := os.MkdirAll(apiDir, 0777); err != nil {
+		t.Error(err)
+	}
 
-	 b, err := proto.Marshal(&m)
-	 if err != nil {
-		 t.Error(err)
-	 }
+	b, err := proto.Marshal(&m)
+	if err != nil {
+		t.Error(err)
+	}
 
-	 if err := ioutil.WriteFile(apiDir + "/marks.pb", b, 0644); err != nil {
-		 t.Error(err)
-	 }
+	if err := ioutil.WriteFile(apiDir+"/marks.pb", b, 0644); err != nil {
+		t.Error(err)
+	}
 
-	 b, err = json.Marshal(&m)
-	 if err != nil {
-		 t.Error(err)
-	 }
+	b, err = json.Marshal(&m)
+	if err != nil {
+		t.Error(err)
+	}
 
-	 if err := ioutil.WriteFile(apiDir + "/marks.json", b, 0644); err != nil {
-		 t.Error(err)
-	 }
+	if err := ioutil.WriteFile(apiDir+"/marks.json", b, 0644); err != nil {
+		t.Error(err)
+	}
 
-	 // GeoJSON files of site for each sensor type.
-	 // This is similar to the sensor type output in stations_test.go
-	 // There is no other sensor type lookup so handle that switching here.
-	 // There is fractionally more work as this is two different installed types
-	 // (antenna and metsensor).
+	// GeoJSON files of site for each sensor type.
+	// This is similar to the sensor type output in stations_test.go
+	// There is no other sensor type lookup so handle that switching here.
+	// There is fractionally more work as this is two different installed types
+	// (antenna and metsensor).
 
-	 out := map[string]*bytes.Buffer{
-		 "metsensor": &bytes.Buffer{},
-		 "gpsantenna": &bytes.Buffer{},
-	 }
+	out := map[string]*bytes.Buffer{
+		"metsensor":  &bytes.Buffer{},
+		"gpsantenna": &bytes.Buffer{},
+	}
 
-	 for k := range out {
-		 out[k].WriteString(`{"type": "FeatureCollection","features": [`)
-	 }
+	for k := range out {
+		out[k].WriteString(`{"type": "FeatureCollection","features": [`)
+	}
 
-	 for _, mark := range m.Marks {
-		 if mark.GetSpan() == nil || mark.GetPoint() == nil || mark.GetNetwork() == nil {
-			 continue
-		 }
+	for _, mark := range m.Marks {
+		if mark.GetSpan() == nil || mark.GetPoint() == nil || mark.GetNetwork() == nil {
+			continue
+		}
 
-		 if mark.InstalledAntenna != nil && len(mark.InstalledAntenna) > 0 {
-			 for _, v := range mark.InstalledAntenna {
-				 writeAntennaProps(mark, v, out["gpsantenna"])
-			 }
-		 }
+		if mark.InstalledAntenna != nil && len(mark.InstalledAntenna) > 0 {
+			for _, v := range mark.InstalledAntenna {
+				writeAntennaProps(mark, v, out["gpsantenna"])
+			}
+		}
 
-		 if mark.InstalledMetSensor != nil && len(mark.InstalledMetSensor) > 0 {
-			 for _,v := range mark.InstalledMetSensor {
-				 writeMetsensorProps(mark, v, out["metsensor"])
-			 }
-		 }
-	 }
+		if mark.InstalledMetSensor != nil && len(mark.InstalledMetSensor) > 0 {
+			for _, v := range mark.InstalledMetSensor {
+				writeMetsensorProps(mark, v, out["metsensor"])
+			}
+		}
+	}
 
-	 for k := range out {
-		 out[k].WriteString(`]}`)
+	for k := range out {
+		out[k].WriteString(`]}`)
 
-		 fn := apiDir + "/" + k + ".geojson"
+		fn := apiDir + "/" + k + ".geojson"
 
-		 if err := ioutil.WriteFile(fn, out[k].Bytes(), 0644); err != nil {
-			 t.Error(err)
-		 }
-	 }
- }
+		if err := ioutil.WriteFile(fn, out[k].Bytes(), 0644); err != nil {
+			t.Error(err)
+		}
+	}
+}
 
 func writeAntennaProps(mark *delta.Mark, antenna *delta.InstalledAntenna, b *bytes.Buffer) {
 	if !bytes.HasSuffix(b.Bytes(), []byte("[")) {
@@ -415,5 +435,3 @@ func writeMetsensorProps(mark *delta.Mark, metsensor *delta.InstalledMetSensor, 
 	b.WriteString(fmt.Sprintf(",\"end\":\"%s\"", time.Unix(metsensor.Span.End, 0).UTC().Format(time.RFC3339)))
 	b.WriteString(`}}`)
 }
-
-
