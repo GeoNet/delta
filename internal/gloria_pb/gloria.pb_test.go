4f387144475a903adfaca981cb053f5eeafa134c
Dan Dooley <acehawkbell@gmail.com>
2017-08-29 13:52:58 +1200

    Adding comment to the protobuf contract and renaming the package for consistency with package name in the gloria project

--- internal/gloria_pb/gloria.pb_test.go
+++ internal/gloria_pb/gloria.pb_test.go
@@ -1,7 +1,6 @@
-package gloria_test
+package gloria_pb
 
 import (
-	"github.com/GeoNet/delta/internal/gloria"
 	"github.com/golang/protobuf/proto"
 	"io/ioutil"
 	"testing"
@@ -10,51 +9,55 @@ import (
 
 // TestProto creates an example Gloria proto file for TAUP.
 func TestProto(t *testing.T) {
-	m := gloria.Mark{
+	m := Mark{
 		Code:        "TAUP",
 		DomesNumber: "50217M001",
-		Point: &gloria.Point{
+		Point: &Point{
 			Longitude: 176.0809947,
 			Latitude:  -38.74271665,
 			Elevation: 427.0279,
 		},
-		DeployedReceiver: []*gloria.DeployedReceiver{
-			&gloria.DeployedReceiver{
-				Receiver: &gloria.Receiver{
+		DeployedReceiver: []*DeployedReceiver{
+			&DeployedReceiver{
+				Receiver: &Receiver{
 					Model:        "TRIMBLE NETR9",
 					SerialNumber: "5033K69574",
-					Firmware: []*gloria.Firmware{
-						&gloria.Firmware{Version: "5.15",
-							Span: &gloria.Span{
+					Firmware: []*Firmware{
+						&Firmware{Version: "5.15",
+							Span: &Span{
 								Start: time.Date(2016, 12, 20, 22, 21, 26, 0, time.UTC).Unix(),
 								End:   time.Date(9999, 1, 1, 0, 0, 0, 0, time.UTC).Unix(),
 							},
 						},
 					},
 				},
-				Span: &gloria.Span{
+				Span: &Span{
 					Start: time.Date(2015, 9, 19, 17, 25, 1, 0, time.UTC).Unix(),
 					End:   time.Date(9999, 1, 1, 0, 0, 0, 0, time.UTC).Unix(),
 				},
 			},
 		},
-		InstalledAntenna: []*gloria.InstalledAntenna{
-			&gloria.InstalledAntenna{
-				Antenna: &gloria.Antenna{
+		InstalledAntenna: []*InstalledAntenna{
+			&InstalledAntenna{
+				Antenna: &Antenna{
 					Model:        "TRM57971.00",
 					SerialNumber: "1441031450",
 				},
-				Offset: &gloria.Offset{
+				Offset: &Offset{
 					East:     0.0,
 					North:    0.0,
 					Vertical: 0.0550,
 				},
-				Span: &gloria.Span{
+				Span: &Span{
 					Start: time.Date(2015, 9, 19, 17, 25, 1, 0, time.UTC).Unix(),
 					End:   time.Date(9999, 1, 1, 0, 0, 0, 0, time.UTC).Unix(),
 				},
 			},
 		},
+		Comment: `Data supplied by the GeoNet project.  GeoNet is core
+funded by EQC, with support from LINZ, and is
+operated by GNS on behalf of EQC and all New Zealanders.
+Contact: www.geonet.org.nz  email: info@geonet.org.nz`,
 	}
 
 	b, err := proto.Marshal(&m)
---
+++ internal/gloria_pb/testdata/taup.pb
@@ -0,0 +1,12 @@
+
+TAUP	50217M001	ëøÔV_CÀÇt2‚—f@ï8EGr°z@"C
+2
+TRIMBLE NETR9
+5033K69574
+5.15æİæÂ€œÌğ¯í´ö¯€œÌğ¯*5
+
+TRM57971.00
+1441031450		)\Âõ(¬?í´ö¯€œÌğ¯:ÑData supplied by the GeoNet project.  GeoNet is core
+funded by EQC, with support from LINZ, and is
+operated by GNS on behalf of EQC and all New Zealanders.
+Contact: www.geonet.org.nz  email: info@geonet.org.nz
