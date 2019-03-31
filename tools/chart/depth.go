b83a3e6042f04ede4c88184e40e961bf72a6a9f0
Mark Chadwick <m.chadwick@gns.cri.nz>
2019-04-01 12:51:38 +1300

    lint cleanup of tools/chart array looping

--- tools/chart/depth.go
+++ tools/chart/depth.go
@@ -34,9 +34,7 @@ func (cp ConfigPage) Depth(base string) ([]Page, error) {
 		if err != nil {
 			return nil, err
 		}
-		for _, s := range list {
-			stns = append(stns, s)
-		}
+		stns = append(stns, list...)
 	}
 
 	sort.Sort(Stations(stns))
