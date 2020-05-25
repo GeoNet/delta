9fa0a3f557fbe14020fb73f665fb237bb3eedccc
Mark Chadwick <m.chadwick@gns.cri.nz>
2020-05-26 11:17:12 +1200

    update trigger scripts

--- run/deploy/10-ac-tools-trigger.sh
+++ run/deploy/10-ac-tools-trigger.sh
@@ -9,11 +9,18 @@ then
     "branch":"master"
     }}'
 
-    curl -s -X POST \
+    STATUSCODE=$(curl --silent --output /dev/stderr --write-out "%{http_code}" -X POST \
        -H "Content-Type: application/json" \
        -H "Accept: application/json" \
        -H "Travis-API-Version: 3" \
        -H "Authorization: token $geonetci_api_token" \
        -d "$body" \
        https://api.travis-ci.com/repo/GeoNet%2Fac-tools/requests
+    )
+
+    if [[ $STATUSCODE -ne 200 ]]
+    then
+            exit 1
+    fi
+
 fi
