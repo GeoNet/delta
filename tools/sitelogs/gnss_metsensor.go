302b3f98ccbaf4ae32f66eca23ad6e9ebe6c6287
Mark Chadwick <m.chadwick@gns.cri.nz>
2020-11-30 15:56:50 +1300

    split metsensors into three components

--- tools/sitelogs/gnss_metsensor.go
+++ tools/sitelogs/gnss_metsensor.go
@@ -1,21 +1,22 @@
 package main
 
 import (
-	"encoding/xml"
+//"encoding/xml"
 )
 
 type GnssMetSensor struct {
-	XMLName xml.Name `xml:"gnssMetSensor"`
-
-	Manufacturer         string
-	MetSensorModel       string
-	SerialNumber         string
-	DataSamplingInterval string
-	Accuracy             string
-	HeightDifftoAnt      string
-	CalibrationDate      string
-	EffectiveDates       string
-	Notes                string
+	MetSensorModel                  string  `xml:"equip:type"`
+	Manufacturer                    string  `xml:"equip:manufacturer"`
+	SerialNumber                    string  `xml:"equip:serialNumber"`
+	HeightDifftoAnt                 string  `xml:"equip:heightDiffToAntenna"`
+	CalibrationDate                 string  `xml:"equip:calibrationDate"`
+	EffectiveDates                  string  `xml:"equip:effectiveDates"`
+	DataSamplingInterval            float64 `xml:"equip:dataSamplingInterval"`
+	AccuracyPercentRelativeHumidity float64 `xml:"equip:accuracy-percentRelativeHumidity,omitempty"`
+	AccuracyHPa                     float64 `xml:"equip:accuracy-hPa,omitempty"`
+	AccuracyDegreesCelcius          float64 `xml:"equip:accuracy-degreesCelcius,omitempty"`
+	Aspiration                      string  `xml:"equip:aspiration"`
+	Notes                           string  `xml:"equip:notes"`
 }
 
 type GnssMetSensors []GnssMetSensor
