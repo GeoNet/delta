0183a81670425ae26ae81cc327838b3ad3cf9a45
Mark Chadwick <m.chadwick@gns.cri.nz>
2022-07-06 18:09:29 +1200

    update

--- internal/stationxml/doc.go
+++ internal/stationxml/doc.go
@@ -1,6 +1,6 @@
-//go:generate bash -c "go run generate/*.go -version v1.0 -schema https://www.fdsn.org/xml/station/fdsn-station-1.0.xsd -insecure -output v1.0"
-//go:generate bash -c "go run generate/*.go -version v1.1 -schema https://www.fdsn.org/xml/station/fdsn-station-1.1.xsd -insecure -output v1.1"
-//go:generate bash -c "go run generate/*.go -version v1.2 -schema https://www.fdsn.org/xml/station/fdsn-station-1.2.xsd -insecure -output v1.2"
+//go:generate bash -c "go run $(find generate -name \"*.go\" -and -not -name \"*_test.go\" -maxdepth 1) -version v1.0 -schema https://www.fdsn.org/xml/station/fdsn-station-1.0.xsd -insecure -output v1.0"
+//go:generate bash -c "go run $(find generate -name \"*.go\" -and -not -name \"*_test.go\" -maxdepth 1) -version v1.1 -schema https://www.fdsn.org/xml/station/fdsn-station-1.1.xsd -insecure -output v1.1"
+//go:generate bash -c "go run $(find generate -name \"*.go\" -and -not -name \"*_test.go\" -maxdepth 1) -version v1.2 -schema https://www.fdsn.org/xml/station/fdsn-station-1.2.xsd -insecure -output v1.2"
 
 // Automatically generate stationxml bindings from the schema files.
 package stationxml
