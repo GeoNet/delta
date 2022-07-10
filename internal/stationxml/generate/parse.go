5329b46ee4edda8ee082cc6e5103838b2827d772
Mark Chadwick <m.chadwick@gns.cri.nz>
2022-07-11 11:05:37 +1200

    add code to render a simple type

--- internal/stationxml/generate/parse.go
+++ internal/stationxml/generate/parse.go
@@ -62,3 +62,32 @@ func (s *Schemas) ParseEnum(pkg string, fn func(string, Enum) error) error {
 
 	return nil
 }
+
+// ParseSimple decode the schema to process simple types
+func (s *Schemas) ParseSimple(pkg string, fn func(string, Simple) error) error {
+
+	for _, v := range s.Schema {
+		if v.TargetNS != s.Namespace {
+			continue
+		}
+
+		for k, t := range v.Types {
+			if k.Space != s.Namespace {
+				continue
+			}
+
+			switch t := t.(type) {
+			case *xsd.SimpleType:
+				if len(t.Restriction.Enum) > 0 {
+					continue
+				}
+
+				if err := fn(xsd.XMLName(t).Local, toSimple(pkg, t)); err != nil {
+					return err
+				}
+			}
+		}
+	}
+
+	return nil
+}
