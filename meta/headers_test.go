1205d80f1937ac481a3db389bcebe84e8257e024
Mark Chadwick <m.chadwick@gns.cri.nz>
2022-12-05 11:56:32 +1300

    add test for extra columns

--- meta/headers_test.go
+++ meta/headers_test.go
@@ -33,7 +33,57 @@ func testHeaderFunc(path string, list List) func(t *testing.T) {
 }
 
 func TestHeaderList(t *testing.T) {
-	t.Run("check headers", testHeaderFunc("testdata/headers.csv", &SampleList{
+
+	t.Run("check reordered headers", testHeaderFunc("testdata/reorder.csv", &SampleList{
+		Sample{
+			Reference: Reference{
+				Code:    "NA000",
+				Network: "MC",
+				Name:    "Ngauruhoe Volcano",
+			},
+			Point: Point{
+				Latitude:  -39.156798,
+				Longitude: 175.631871,
+				Elevation: 2197.0,
+				Depth:     0,
+				Datum:     "WGS84",
+
+				latitude:  "-39.156798",
+				longitude: "175.631871",
+				elevation: "2197.0",
+				depth:     "",
+			},
+			Span: Span{
+				Start: time.Date(2003, time.July, 17, 0, 0, 0, 0, time.UTC),
+				End:   time.Date(9999, time.January, 1, 0, 0, 0, 0, time.UTC),
+			},
+		},
+		Sample{
+			Reference: Reference{
+				Code:    "NA001",
+				Network: "MC",
+				Name:    "Ngauruhoe crater inner rim north-west fumarole field",
+			},
+			Point: Point{
+				Latitude:  -39.1562653309,
+				Longitude: 175.631464065,
+				Elevation: 2280.0,
+				Depth:     0,
+				Datum:     "WGS84",
+
+				latitude:  "-39.1562653309",
+				longitude: "175.631464065",
+				elevation: "2280",
+				depth:     "",
+			},
+			Span: Span{
+				Start: time.Date(1978, time.July, 1, 0, 0, 0, 0, time.UTC),
+				End:   time.Date(9999, time.January, 1, 0, 0, 0, 0, time.UTC),
+			},
+		},
+	}))
+
+	t.Run("check extra headers", testHeaderFunc("testdata/extra.csv", &SampleList{
 		Sample{
 			Reference: Reference{
 				Code:    "NA000",
