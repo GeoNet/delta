dad4f24f556bbfed3c526bfee00a9d313906e43e
Mark Chadwick <m.chadwick@gns.cri.nz>
2022-12-12 12:10:13 +1300

    update tests to remove exec call, replace with cmp

--- tools/impact/impact_test.go
+++ tools/impact/impact_test.go
@@ -3,10 +3,10 @@ package main
 import (
 	"encoding/json"
 	"io/ioutil"
-	"os"
-	"os/exec"
 	"strings"
 	"testing"
+
+	"github.com/google/go-cmp/cmp"
 )
 
 func TestBuild_Network(t *testing.T) {
@@ -29,42 +29,6 @@ func TestBuild_Network(t *testing.T) {
 
 	// compare stored with computed
 	if strings.TrimSpace(string(b1)) != strings.TrimSpace(string(b2)) {
-		t.Error("**** impact json mismatch ****")
-
-		f1, err := ioutil.TempFile(os.TempDir(), "tmp")
-		if err != nil {
-			t.Fatal(err)
-		}
-		defer os.Remove(f1.Name())
-		if _, err := f1.Write(b1); err != nil {
-			t.Fatal(err)
-		}
-
-		f2, err := ioutil.TempFile(os.TempDir(), "tmp")
-		if err != nil {
-			t.Fatal(err)
-		}
-		defer os.Remove(f2.Name())
-		if _, err := f2.Write(b2); err != nil {
-			t.Fatal(err)
-		}
-
-		cmd := exec.Command("diff", "-c", f1.Name(), f2.Name())
-		stdout, err := cmd.StdoutPipe()
-		if err != nil {
-			t.Fatal(err)
-		}
-		err = cmd.Start()
-		if err != nil {
-			t.Fatal(err)
-		}
-		diff, err := ioutil.ReadAll(stdout)
-		if err != nil {
-			t.Fatal(err)
-		}
-		t.Error(string(diff))
-		if err := cmd.Wait(); err != nil {
-			t.Fatal(err)
-		}
+		t.Errorf("unexpected content -got/+exp\n%s", cmp.Diff(string(b1), string(b2)))
 	}
 }
