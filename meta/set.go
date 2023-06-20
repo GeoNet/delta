21ed2bcbde8e896a25666d5239d6b38ef091fb2d
Mark Chadwick <m.chadwick@gns.cri.nz>
2023-06-20 17:49:22 +1200

    feat(notes): add a environment notes table

--- meta/set.go
+++ meta/set.go
@@ -48,6 +48,7 @@ const (
 	ConstituentsFile = "environment/constituents.csv"
 	FeaturesFile     = "environment/features.csv"
 	GaugesFile       = "environment/gauges.csv"
+	NotesFile        = "environment/notes.csv"
 	PlacenamesFile   = "environment/placenames.csv"
 	VisibilityFile   = "environment/visibility.csv"
 
@@ -102,6 +103,7 @@ type Set struct {
 	constituents ConstituentList
 	features     FeatureList
 	gauges       GaugeList
+	notes        NoteList
 	placenames   PlacenameList
 	visibilities VisibilityList
 
@@ -150,6 +152,7 @@ func (s *Set) files() map[string]List {
 		ConstituentsFile: &s.constituents,
 		FeaturesFile:     &s.features,
 		GaugesFile:       &s.gauges,
+		NotesFile:        &s.notes,
 		PlacenamesFile:   &s.placenames,
 		VisibilityFile:   &s.visibilities,
 
