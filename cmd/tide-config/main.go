55f621ca19c2c3184de5d70fb50835cd8af9f44b
Mark Chadwick <m.chadwick@gns.cri.nz>
2024-06-01 13:18:15 +1200

    chore: add a tide-config application to replace tide
    This replaces the existing tool "tide" to allow for the removal of metadb

---
+++ cmd/tide-config/main.go
@@ -0,0 +1,68 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/GeoNet/delta"
+)
+
+type Settings struct {
+	base   string
+	header string
+	footer string
+}
+
+func main() {
+
+	var settings Settings
+
+	flag.Usage = func() {
+		fmt.Fprintf(os.Stderr, "\n")
+		fmt.Fprintf(os.Stderr, "Provide tidal templating\n")
+		fmt.Fprintf(os.Stderr, "\n")
+		fmt.Fprintf(os.Stderr, "Usage:\n")
+		fmt.Fprintf(os.Stderr, "\n")
+		fmt.Fprintf(os.Stderr, "  %s [options] [templates ....]\n", os.Args[0])
+		fmt.Fprintf(os.Stderr, "\n")
+		fmt.Fprintf(os.Stderr, "General Options:\n")
+		fmt.Fprintf(os.Stderr, "\n")
+		flag.PrintDefaults()
+	}
+
+	flag.StringVar(&settings.base, "base", "", "base of delta files on disk")
+	flag.StringVar(&settings.header, "header", "", "output header before processing any templates")
+	flag.StringVar(&settings.footer, "footer", "", "output footer after all templates have been processed")
+
+	flag.Parse()
+
+	set, err := delta.NewBase(settings.base)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if settings.header != "" {
+		fmt.Fprintln(os.Stdout, settings.header)
+	}
+
+	// process each template given on the command line
+	for _, file := range flag.Args() {
+		conf, err := os.ReadFile(file)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		config, err := Parse(set, conf)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		fmt.Fprintln(os.Stdout, string(config))
+	}
+
+	if settings.footer != "" {
+		fmt.Fprintln(os.Stdout, settings.footer)
+	}
+}
