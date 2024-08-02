3d8828ad6fde70a619da83529767114d3cb92d58
Howard Wu <junghao@gmail.com>
2024-08-02 13:33:05 +1200

    feat: Skeleton file generating from bnc-config

---
+++ cmd/bnc-config/skl_test.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"bytes"
+	"os"
+	"testing"
+	"time"
+
+	"github.com/GeoNet/delta"
+)
+
+func TestSkeleton(t *testing.T) {
+	// set recovers the delta tables
+	set, err := delta.NewBase("./testdata")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	tm := time.Date(2024, 1, 1, 0, 0, 0, 0, time.UTC)
+	b, err := skeleton("AVLN", set, tm.UTC().Unix())
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	f, err := os.ReadFile("./testdata/AVLN00NZL.SKL")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if !bytes.Equal([]byte(b), f) {
+		t.Errorf("generated skeleton file does not match:\n\n%s", b)
+	}
+}
