89108734637aaff68533751248e4d11e9da3c97c
Howard Wu <junghao@gmail.com>
2024-08-08 14:45:35 +1200

    feat: use generic header upon error

--- cmd/bnc-config/main.go
+++ cmd/bnc-config/main.go
@@ -6,7 +6,6 @@ import (
 	"log"
 	"os"
 	"path/filepath"
-	"slices"
 	"strings"
 	"time"
 
@@ -15,13 +14,13 @@ import (
 )
 
 type Settings struct {
-	base        string   // delta base directory
-	common      string   // ntrip common files directory
-	input       string   // ntrip input files directory
-	extra       bool     // add aliases to mounts list
-	output      string   // optional output file
-	sklPath     string   // optional path to write skeleton files
-	skippingSkl []string // optional list of marks to skip skeleton file generation
+	base            string // delta base directory
+	common          string // ntrip common files directory
+	input           string // ntrip input files directory
+	extra           bool   // add aliases to mounts list
+	output          string // optional output file
+	sklPath         string // optional path to write skeleton files
+	fallbackSummary string // optional path-filename to write skeleton fallback summary info
 }
 
 func main() {
@@ -47,14 +46,8 @@ func main() {
 	flag.BoolVar(&settings.extra, "extra", false, "add aliases to mounts list")
 	flag.StringVar(&settings.output, "output", "", "optional output file")
 	flag.StringVar(&settings.sklPath, "skl", "", "optional path to write skeleton files")
-	var skippingSkl string
-	flag.StringVar(&skippingSkl, "skip-skl", "", "optional comma separated list of marks to skip skeleton file generation")
-
+	flag.StringVar(&settings.fallbackSummary, "fallback-summary", "", "optional path-filename to record sites using generic skeleton file")
 	flag.Parse()
-
-	if skippingSkl != "" {
-		settings.skippingSkl = strings.Split(skippingSkl, ",")
-	}
 
 	// set recovers the delta tables
 	set, err := delta.NewBase(settings.base)
@@ -90,19 +83,26 @@ func main() {
 
 	// generate skeleton file for each mount
 	if settings.sklPath != "" {
+		fallbacks := make([]string, 0)
 		t := time.Now().UTC().Unix() // skeleton needs a reference time for the installations
 		for _, m := range config.Mounts {
-			if !slices.Contains(settings.skippingSkl, m.Mark) {
-				if s, err := skeleton(m.Mark, set, t); err == nil {
-					if len(s) > 0 { // empty means no valid mark during the reference time, simply skip without error
-						err = os.WriteFile(filepath.Join(settings.sklPath, fmt.Sprintf("%s00NZL.SKL", m.Mark)), []byte(s), 0600)
-						if err != nil {
-							log.Fatalf("couldn't write skeleton file: %s", err)
-						}
-					}
-				} else {
-					log.Fatalf("couldn't create skeleton: %s", err)
+			// note: skeleton() will return with generic header content when error occured
+			s, err := skeleton(m.Mark, set, t)
+			if err != nil {
+				fallbacks = append(fallbacks, m.Mark) // we'll output the list in the end
+			}
+			err = os.WriteFile(filepath.Join(settings.sklPath, fmt.Sprintf("%s00NZL.SKL", m.Mark)), []byte(s), 0600)
+			if err != nil {
+				log.Fatalf("couldn't write skeleton file: %s", err)
+			}
+		}
+		if len(fallbacks) > 0 {
+			if settings.fallbackSummary != "" {
+				if err = os.WriteFile(settings.fallbackSummary, []byte(strings.Join(fallbacks, ",")), 0600); err != nil {
+					log.Fatalf("couldn't write fallback summary file: %s", err)
 				}
+			} else {
+				fmt.Printf("marks using generic file: %s\n", strings.Join(fallbacks, ","))
 			}
 		}
 	}
