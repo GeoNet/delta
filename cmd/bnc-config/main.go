494a57510876eef2561e82aab09c4f99da572ed4
Howard Wu <junghao@gmail.com>
2024-08-29 11:09:33 +1200

    feat: bnc-config: Skeleton files use generic header upon error

--- cmd/bnc-config/main.go
+++ cmd/bnc-config/main.go
@@ -6,7 +6,6 @@ import (
 	"log"
 	"os"
 	"path/filepath"
-	"slices"
 	"strings"
 	"time"
 
@@ -15,13 +14,12 @@ import (
 )
 
 type Settings struct {
-	base        string   // delta base directory
-	common      string   // ntrip common files directory
-	input       string   // ntrip input files directory
-	extra       bool     // add aliases to mounts list
-	output      string   // optional output file
-	sklPath     string   // optional path to write skeleton files
-	skippingSkl []string // optional list of marks to skip skeleton file generation
+	base    string // delta base directory
+	common  string // ntrip common files directory
+	input   string // ntrip input files directory
+	output  string // optional output file
+	sklPath string // optional path to write skeleton files
+	summary string // optional path-filename to write skeleton summary info
 }
 
 func main() {
@@ -44,17 +42,10 @@ func main() {
 	flag.StringVar(&settings.base, "base", "", "delta base directory for config files")
 	flag.StringVar(&settings.common, "common", "", "ntrip common csv file directory")
 	flag.StringVar(&settings.input, "input", "", "ntrip input csv config file directory")
-	flag.BoolVar(&settings.extra, "extra", false, "add aliases to mounts list")
 	flag.StringVar(&settings.output, "output", "", "optional output file")
 	flag.StringVar(&settings.sklPath, "skl", "", "optional path to write skeleton files")
-	var skippingSkl string
-	flag.StringVar(&skippingSkl, "skip-skl", "", "optional comma separated list of marks to skip skeleton file generation")
-
+	flag.StringVar(&settings.summary, "summary", "", "optional filename to output summary info for skeleton file")
 	flag.Parse()
-
-	if skippingSkl != "" {
-		settings.skippingSkl = strings.Split(skippingSkl, ",")
-	}
 
 	// set recovers the delta tables
 	set, err := delta.NewBase(settings.base)
@@ -68,7 +59,7 @@ func main() {
 	}
 
 	// generate the configuration structures
-	config, err := NewConfig(set, caster, settings.extra)
+	config, err := NewConfig(set, caster)
 	if err != nil {
 		log.Fatalf("unable to build config: %v", err)
 	}
@@ -90,19 +81,30 @@ func main() {
 
 	// generate skeleton file for each mount
 	if settings.sklPath != "" {
+		// we'll output the list in the end
+		fallbacks := make([]string, 0)
+		successes := make([]string, 0)
 		t := time.Now().UTC().Unix() // skeleton needs a reference time for the installations
 		for _, m := range config.Mounts {
-			if !slices.Contains(settings.skippingSkl, m.Mark) {
-				if s, err := skeleton(m.Mark, set, t); err == nil {
-					if len(s) > 0 { // empty means no valid mark during the reference time, simply skip without error
-						err = os.WriteFile(filepath.Join(settings.sklPath, fmt.Sprintf("%s00NZL.SKL", m.Mark)), []byte(s), 0600)
-						if err != nil {
-							log.Fatalf("couldn't write skeleton file: %s", err)
-						}
-					}
-				} else {
-					log.Fatalf("couldn't create skeleton: %s", err)
-				}
+			// note: skeleton() will return with generic header content when error occured
+			s, err := skeleton(m.Mark, set, t)
+			if err != nil {
+				fallbacks = append(fallbacks, m.Mark)
+			} else {
+				successes = append(successes, m.Mark)
+			}
+			err = os.WriteFile(filepath.Join(settings.sklPath, fmt.Sprintf("%s00NZL.SKL", m.Mark)), []byte(s), 0600)
+			if err != nil {
+				log.Fatalf("couldn't write skeleton file: %s", err)
+			}
+		}
+		if settings.summary != "" {
+			// output a summary info of skeleton generating
+			var output strings.Builder
+			output.WriteString("successes: " + fmt.Sprintln(successes))
+			output.WriteString("generic headers: " + fmt.Sprintln(fallbacks))
+			if err = os.WriteFile(settings.summary, []byte(output.String()), 0600); err != nil {
+				log.Fatalf("couldn't write fallback summary file: %s", err)
 			}
 		}
 	}
--- cmd/bnc-config/skl_test.go
+++ cmd/bnc-config/skl_test.go
@@ -31,3 +31,26 @@ func TestSkeleton(t *testing.T) {
 		t.Errorf("generated skeleton file does not match(%d vs %d):\n%s", len(b), len(f), b)
 	}
 }
+
+// tests the case where we unable to generate a skeleton file by given metadata,
+// and the generic skeleton file is used instead.
+func TestGenericSkeleton(t *testing.T) {
+	set, err := delta.NewBase("./testdata")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// set the reference time to before the mark's install time
+	tm := time.Date(2005, 1, 1, 0, 0, 0, 0, time.UTC)
+	b, err := skeleton("AVLN", set, tm.UTC().Unix())
+	if err == nil {
+		t.Fatalf("expected to get error but got nil")
+	}
+	f, err := os.ReadFile("./testdata/AVLN-generic.SKL")
+	if err != nil {
+		t.Fatal(err)
+	}
+	if !bytes.Equal([]byte(b), f) {
+		t.Errorf("generated skeleton file does not match(%d vs %d)", len(b), len(f))
+	}
+}
