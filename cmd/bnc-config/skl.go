5d16401e24d20b148ebf615ea067e6078d8b33df
Howard Wu <junghao@gmail.com>
2024-08-29 11:09:33 +1200

    feat: bnc-config: Skeleton files use generic header upon error

--- cmd/bnc-config/skl.go
+++ cmd/bnc-config/skl.go
@@ -11,15 +11,32 @@ var geo = ellipsoid.Init("WGS84", ellipsoid.Degrees, ellipsoid.Meter, ellipsoid.
 
 // genetrate skeleton file for a site,
 // based on the receiver, firmware, and antenna metadata at a given time (e.g. now)
-func skeleton(code string, set *meta.Set, ts int64) (string, error) {
+func skeleton(code string, set *meta.Set, ts int64) (content string, err error) {
 	var mark meta.Mark
+
+	defer func() {
+		if err != nil {
+			// if any error occurs, compose generic header + distribution comment as the result
+			// however, still keep the error
+			content = generic_header
+			if mark.Network == "LI" {
+				content += linzComment
+			} else {
+				content += geonetComment
+			}
+		}
+	}()
+
 	mark, ok := set.Mark(code)
 	if !ok {
-		return "", fmt.Errorf("no mark found for %s", code)
+		err = fmt.Errorf("no mark found for %s", code)
+		return
 	}
 	if !inWindow(ts, mark.Span) {
-		return "", nil // it's fine if there's no valid mark for the reference time
+		err = fmt.Errorf("no valid mark found for this time period for %s", code)
+		return
 	}
+
 	receivers := set.DeployedReceivers()
 	var dr meta.DeployedReceiver
 	for _, r := range receivers {
@@ -30,7 +47,8 @@ func skeleton(code string, set *meta.Set, ts int64) (string, error) {
 	}
 
 	if dr.Span.Start.IsZero() { // empty value
-		return "", fmt.Errorf("no effective deployed receiver version found for this time for %s", mark.Code)
+		err = fmt.Errorf("no effective deployed receiver version found for this time for %s", mark.Code)
+		return
 	}
 
 	x, y, z := geo.ToECEF(mark.Latitude, mark.Longitude, mark.Elevation)
@@ -44,7 +62,8 @@ func skeleton(code string, set *meta.Set, ts int64) (string, error) {
 	}
 
 	if ifirm.Span.Start.IsZero() {
-		return "", fmt.Errorf("no effective firmware version found for this time for %s", mark.Code)
+		err = fmt.Errorf("no effective firmware version found for this time for %s", mark.Code)
+		return
 	}
 
 	antennas := set.InstalledAntennas()
@@ -57,7 +76,8 @@ func skeleton(code string, set *meta.Set, ts int64) (string, error) {
 	}
 
 	if ia.Span.Start.IsZero() {
-		return "", fmt.Errorf("no effective installed antenna found for this time for %s", mark.Code)
+		err = fmt.Errorf("no effective installed antenna found for this time for %s", mark.Code)
+		return
 	}
 
 	radomes := set.InstalledRadomes()
@@ -91,7 +111,7 @@ func skeleton(code string, set *meta.Set, ts int64) (string, error) {
 		domesNumber = monument.DomesNumber
 	}
 
-	s := fmt.Sprintf(skeletonFormat,
+	content = fmt.Sprintf(skeletonFormat,
 		fmt.Sprintf("%s00NZL", code),       // MARKER NAME
 		domesNumber,                        // MARKER NUMBER
 		dr.Serial, dr.Model, ifirm.Version, // REC # / TYPE / VERS
@@ -99,12 +119,12 @@ func skeleton(code string, set *meta.Set, ts int64) (string, error) {
 		x, y, z, //APPROX POSITION XYZ
 		ia.Offset.Vertical, ia.Offset.East, ia.Offset.North) // ANTENNA: DELTA H/E/N
 	if mark.Network == "LI" {
-		s += linzComment
+		content += linzComment
 	} else {
-		s += geonetComment
+		content += geonetComment
 	}
 
-	return s, nil
+	return
 }
 
 const skeletonFormat = `                    OBSERVATION DATA    M (Mixed)           RINEX VERSION / TYPE
@@ -114,6 +134,24 @@ const skeletonFormat = `                    OBSERVATION DATA    M (Mixed)       
 %-20s%-16s%-4s                    ANT # / TYPE
 %-20.4f%-20.4f%-20.4fAPPROX POSITION XYZ
 %-20.4f%-20.4f%-20.4fANTENNA: DELTA H/E/N
+GEODETIC                                                    MARKER TYPE
+GeoNet              GNS                                     OBSERVER / AGENCY
+G   15 C1C C1X C2W C2X C5X L1C L1X L2W L2X L5X S1C S1X S2W  SYS / # / OBS TYPES
+       S2X S5X                                              SYS / # / OBS TYPES
+E   15 C1X C5X C6X C7X C8I L1X L5X L6X L7X L8I S1X S5X S6X  SYS / # / OBS TYPES
+       S7X S8I                                              SYS / # / OBS TYPES
+R   15 C1C C1P C2C C2P C3X L1C L1P L2C L2P L3X S1C S1P S2C  SYS / # / OBS TYPES
+       S2P S3X                                              SYS / # / OBS TYPES
+C   15 C1X C2I C5X C6I C7I L1X L2I L5X L6I L7I S1X S2I S5X  SYS / # / OBS TYPES
+       S6I S7I                                              SYS / # / OBS TYPES
+J   18 C1C C1X C1Z C2X C5X C6L L1C L1X L1Z L2X L5X L6L S1C  SYS / # / OBS TYPES
+       S1X S1Z S2X S5X S6L                                  SYS / # / OBS TYPES
+G L2X -0.25000                                              SYS / PHASE SHIFT
+R L1P  0.25000                                              SYS / PHASE SHIFT
+R L2C -0.25000                                              SYS / PHASE SHIFT
+J L2X  0.25000                                              SYS / PHASE SHIFT
+`
+const generic_header = `                    OBSERVATION DATA    M (Mixed)           RINEX VERSION / TYPE
 GEODETIC                                                    MARKER TYPE
 GeoNet              GNS                                     OBSERVER / AGENCY
 G   15 C1C C1X C2W C2X C5X L1C L1X L2W L2X L5X S1C S1X S2W  SYS / # / OBS TYPES
